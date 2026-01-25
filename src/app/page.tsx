5898ce2351e0554915d8af9c3bd305db0d5cab44
Your Name <you@example.com>
2026-01-25 13:51:00 +0530

    new code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <h1>CRM Next.js</h1>
+      <Button>Get Started</Button>
     </>
   );
 }
