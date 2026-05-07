207c96bdb3c0464ec3fb292c551f932a9b5d8d81
VISHAL KUSHAWAH <kushawahavishal29@gmail.com>
2026-05-07 06:47:28 +0000

    new 1

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,18 +1,17 @@
-// lib/mongodb.ts
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable");
+  throw new Error("MONGODB_URI is missing in .env.local");
 }
 
 export const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return; // Agar connect hai toh wapis mat karo
+  
   try {
-    if (mongoose.connection.readyState >= 1) return;
     await mongoose.connect(MONGODB_URI);
-    console.log("MongoDB Connected");
   } catch (error) {
-    console.error("MongoDB Connection Error:", error);
+    console.error("Connection failed", error);
   }
 };
